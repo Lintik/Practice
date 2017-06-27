1d3b12af8cb7482e4ccb1d88eaa4100e6f302c66
Marco <domandominguez@hotmail.com>
2017-06-27 01:10:09 -0700

    Create code1.cs

---
+++ removeKFromList/code1.cs
@@ -0,0 +1,17 @@
+ListNode<int> removeKFromList(ListNode<int> l, int k) {
+    List<int> a = new List<int>();
+    while(true){
+        if (l == null) break;
+        if (l.value != k) a.Add(l.value);
+        l = l.next;
+    }
+    ListNode<int> r = null;
+    ListNode<int> t = null;
+    for (int i = a.Count-1; i >=0; i--){
+        r = t;
+        t = new ListNode<int>();
+        t.value = a[i];
+        t.next = r;
+    }
+    return t;
+}
